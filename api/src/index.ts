99938c2337d54c1db01e462133d7e3da48c13bf8
Mateo Lopera <mlopera@prevalentware.com>
2024-06-11 11:14:19 -0500

    Merge con cambio de middlewares

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,7 @@
 import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 import { getDB } from './db';
 import { getSession } from './auth/getSession';
-
 import { resolverArray, typesArray } from './models';
 import { middlewareFunctions, requestHandler } from './middleware';
 import { Context } from './types';
@@ -13,12 +12,17 @@ const server = new ApolloServer<Context>({
   introspection: true,
 });
 
-export const handler = startServerAndCreateLambdaHandler(server, requestHandler, {
-  context: async ({ event }) => {
-    const db = await getDB();
-    const sessionToken: string | undefined = event.headers['next-auth.session-token'];
-    const session = await getSession(db, sessionToken);
-    return { db, session };
-  },
-  middleware: middlewareFunctions,
-});
+export const handler = startServerAndCreateLambdaHandler(
+  server,
+  requestHandler,
+  {
+    context: async ({ event }) => {
+      const db = await getDB();
+      const sessionToken: string | undefined =
+        event.headers['next-auth.session-token'];
+      const session = await getSession(db, sessionToken);
+      return { db, session };
+    },
+    middleware: middlewareFunctions,
+  }
+);
